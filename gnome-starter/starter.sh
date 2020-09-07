5d9f7e7d1dbb86f4a0b039cb199e05953bce34bc
Mišo Barišić <50531162+misobarisic@users.noreply.github.com>
2020-09-07 12:56:00 +0200

    Update

--- gnome-starter/starter.sh
+++ gnome-starter/starter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 chmod 777 *.sh
-git clone https://github.com/misobarisic.com/linux-look
+git clone https://github.com/misobarisic.com/linux-look.git
 cd linux-look
 mv icons/* ~/.icons/
 mv themes/* ~/.themes/
